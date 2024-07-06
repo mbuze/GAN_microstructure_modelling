48a71dca6dd9f1b3724b11263b4476ce6fc4b5c5
mbuze <maciej.buze@gmail.com>
2024-07-06 11:59:48 +0100

    first attempt

---
+++ demo.py
@@ -0,0 +1,8 @@
+import os
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import PIL
+from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
